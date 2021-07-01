765dc616663aeb192b9f0f266b27f7155c94d65f
nilnilnilnil <baixiejia@hotmail.com>
2021-07-01 16:59:01 +1000

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,27 @@
+module.exports = async (req, res) => {
+  const { body } = req;
+  const contactAddress = "baixiejia@hotmail.com"
+
+const mailer = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: xiejiabai,
+    pass: u87441194,
+  },
+})
+
+mailer.sendMail(
+    {
+      from: ${body.from},
+      to: [contactAddress],
+      subject:  "[No subject]",
+      html: "[No message]",
+    },
+    function(err, info) {
+      if (err) res.send(err)
+      res.send(" success: true ")
+    }
+  )
+
+  res.send(`Hello ${body.from}!`);
+};
