62fc7d4a7eaa7a28e2cc30e363bb074af817a9ce
nilnilnilnil <baixiejia@hotmail.com>
2021-07-01 16:43:33 +1000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 module.exports = async (req, res) => {
   const { body } = req;
+  const contactAddress = "baixiejia@hotmail.com"
+
+const mailer = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: xiejiabai,
+    pass: u87441194,
+  },
+})
+
   res.send(`Hello ${body.name}, you just parsed the request body!`);
 };
